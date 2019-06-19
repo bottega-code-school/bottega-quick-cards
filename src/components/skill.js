6e699925df2c5cfbb46f6e2c83005e47f02f71bb
neilmyers <mmyersneil@gmail.com>
2019-06-19 10:41:56 -0600

    skill level gradient

--- src/components/skill.js
+++ src/components/skill.js
@@ -3,15 +3,13 @@ import React from "react";
 const Skill = props => {
   return (
     <div className="skill-wrapper">
-      <div className="skill-bar-wrapper">
-        <div
-          className="skill-bar"
-          style={{
-            width: `${props.width * 20}%`
-          }}
-        />
-        <span className="skill">{props.skill}</span>
-      </div>
+      <div
+        className="skill-bar"
+        style={{
+          width: `${100 - props.width * 20}%`
+        }}
+      />
+      <span className="skill">{props.skill}</span>
     </div>
   );
 };
