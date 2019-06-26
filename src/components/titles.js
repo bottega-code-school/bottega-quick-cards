bd827a04eb77d1a278bcfc788ad35480b925dcb0
neilmyers <mmyersneil@gmail.com>
2019-06-26 10:46:28 -0600

    shortened titles

--- src/components/titles.js
+++ src/components/titles.js
@@ -5,10 +5,9 @@ const Titles = () => {
     <div className="titles">
       <div className="main-title">
         <h1>Bottega</h1>
-        <h2>Production Ready, Certified Engineers</h2>
       </div>
       <div>
-        <h3>These engineers have been certified by:</h3>
+        <h3>Production ready engineers, Certified by:</h3>
         <ul>
           <li>The American Council on Education</li>
           <li>NorthWest Accreditation Commission</li>
