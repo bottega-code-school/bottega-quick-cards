ef53ad8a4284f7940341474b61e63f20c1301914
neilmyers <mmyersneil@gmail.com>
2019-06-26 10:35:28 -0600

    removed accidental side scrolling

--- src/components/titles.js
+++ src/components/titles.js
@@ -7,14 +7,16 @@ const Titles = () => {
         <h1>Bottega</h1>
         <h2>Production Ready, Certified Engineers</h2>
       </div>
-      <h3>These engineers have been certified by:</h3>
-      <ul>
-        <li>The American Council on Education</li>
-        <li>NorthWest Accreditation Commission</li>
-        <li>AdvancEd</li>
-        <li>Distance Education Accrediting Commission</li>
-        <li>Department of Education Approved</li>
-      </ul>
+      <div>
+        <h3>These engineers have been certified by:</h3>
+        <ul>
+          <li>The American Council on Education</li>
+          <li>NorthWest Accreditation Commission</li>
+          <li>AdvancEd</li>
+          <li>Distance Education Accrediting Commission</li>
+          <li>Department of Education Approved</li>
+        </ul>
+      </div>
     </div>
   );
 };
