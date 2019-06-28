3f8d39f002097560afe175c0ba8a73b7972a5feb
neilmyers <mmyersneil@gmail.com>
2019-06-28 10:25:20 -0600

    rubric descriptions

--- src/components/rubric/rubric.js
+++ src/components/rubric/rubric.js
@@ -6,23 +6,23 @@ const Rubric = () => {
     <ol className="rubric">
       <RubricItem
         title="Basic"
-        description="Has a primary understanding of programming concepts"
+        description="Has a primary understanding of programming concepts and scructures"
       />
       <RubricItem
         title="Operational"
-        description="Can survive in a work environment"
+        description="Can perform basic tasks and solve real world problems"
       />
       <RubricItem
         title="Supervised"
-        description="Able to complete tasks with adequate supervision and help"
+        description="Functional in a work environment"
       />
       <RubricItem
         title="Outcome Based"
-        description="Able to complete difficult tasks when assigned and given time"
+        description="Able to complete difficult tasks when assigned"
       />
       <RubricItem
         title="Proactive"
-        description="Able to actively look for difficult problems and tasks to complete"
+        description="Actively looks for difficult problems and tasks to complete"
       />
     </ol>
   );
