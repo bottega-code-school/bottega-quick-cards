be9a7632ff3396cde8d6071e93d7eacf2cdcf58e
neilmyers <mmyersneil@gmail.com>
2019-09-03 14:38:54 -0600

    single student page task list

--- src/components/cards/single-student.js
+++ src/components/cards/single-student.js
@@ -60,7 +60,43 @@ const SingleStudent = props => {
     <div className="home">
       <Navbar />
       <Rubric pic="/assets/chevron.png" />
-      <StudentCard key={student.id} student={student} skillArray={skillArray} />
+      <div className="single-student-wrapper">
+        <StudentCard
+          key={student.id}
+          student={student}
+          skillArray={skillArray}
+        />
+        <div className="task-list-wrapper">
+          <h3>Tasks you can ask me to do:</h3>
+          <ul>
+            <li>- Create cookie based API authentication system</li>
+            <li>- Create JWT based API authentication system</li>
+            <li>- Implement authorization rules for various user types</li>
+            <li>- Connect JavaScript front end with server side backend</li>
+            <li>- Understand the request/response lifecycle</li>
+            <li>- Convert design to HTML/CSS code</li>
+            <li>- Build SQL queries</li>
+            <li>- Build out CRUD functionality</li>
+            <li>- Create MVC based applications</li>
+            <li>- Create micro service based applications</li>
+            <li>
+              - Work with various data structures, such as arrays and key value
+              collections
+            </li>
+            <li>- Build front end and backend applications from scratch</li>
+            <li>- Add features to our existing applications</li>
+            <li>- Read and work with automated tests</li>
+            <li>- Build out SPA based routing systems</li>
+            <li>- Build out API based routing actions</li>
+            <li>- Implement infinite scrolling based pagination</li>
+            <li>- Work with Scss imports, variables, nesting, and mixins</li>
+            <li>- Deploy applications to the web</li>
+            <li>- Work with CORS policies</li>
+            <li>- Utilize debuggers</li>
+            <li>- Utilize the browser development tools</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
